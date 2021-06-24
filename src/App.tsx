f78eaa85b934e8f93ed722435f05ea97b1e95053
zhanghaiyu <2030564240@qq.com>
2021-06-24 18:36:14 +0800

    commit -m 表单登录效果

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginScreen from './screen/login';
 import ProjectListScreen from './screen/project-list';
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen/>
+      <LoginScreen/>
+      {/* <ProjectListScreen/> */}
 
     </div>
   );
